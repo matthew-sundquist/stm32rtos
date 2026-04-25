99a13155f1b85f42df18ec3c21b7c6b6131b35c1
Matthew Sundquist <jwp896@usask.ca>
2026-04-24 19:48:12 -0600

    inital commit

---
+++ Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/Matthew\ Sundquist/STM32CubeIDE/workspace_2.1.1/sunny_rtos/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h \
+ C:/Users/Matthew\ Sundquist/STM32CubeIDE/workspace_2.1.1/sunny_rtos/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h \
+ C:/Users/Matthew\ Sundquist/STM32CubeIDE/workspace_2.1.1/sunny_rtos/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/Matthew\ Sundquist/STM32CubeIDE/workspace_2.1.1/sunny_rtos/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/Matthew\ Sundquist/STM32CubeIDE/workspace_2.1.1/sunny_rtos/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/Matthew\ Sundquist/STM32CubeIDE/workspace_2.1.1/sunny_rtos/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Matthew\ Sundquist/STM32CubeIDE/workspace_2.1.1/sunny_rtos/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/Matthew\ Sundquist/STM32CubeIDE/workspace_2.1.1/sunny_rtos/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h
+C:/Users/Matthew\ Sundquist/STM32CubeIDE/workspace_2.1.1/sunny_rtos/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h:
+C:/Users/Matthew\ Sundquist/STM32CubeIDE/workspace_2.1.1/sunny_rtos/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h:
+C:/Users/Matthew\ Sundquist/STM32CubeIDE/workspace_2.1.1/sunny_rtos/Drivers/CMSIS/Include/core_cm4.h:
+C:/Users/Matthew\ Sundquist/STM32CubeIDE/workspace_2.1.1/sunny_rtos/Drivers/CMSIS/Include/cmsis_version.h:
+C:/Users/Matthew\ Sundquist/STM32CubeIDE/workspace_2.1.1/sunny_rtos/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/Matthew\ Sundquist/STM32CubeIDE/workspace_2.1.1/sunny_rtos/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/Matthew\ Sundquist/STM32CubeIDE/workspace_2.1.1/sunny_rtos/Drivers/CMSIS/Include/mpu_armv7.h:
+C:/Users/Matthew\ Sundquist/STM32CubeIDE/workspace_2.1.1/sunny_rtos/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
