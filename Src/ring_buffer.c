cf9fa2a19328b74404c6756609dca86b36dbe114
Matthew Sundquist <jwp896@usask.ca>
2026-06-30 00:11:16 -0600

    finished initial implementation of ring buffer

--- Src/ring_buffer.c
+++ Src/ring_buffer.c
@@ -6,6 +6,10 @@ static inline size_t get_total_elements(const uint32_t head, const uint32_t tail
 
 static inline size_t get_free_space(const uint32_t head, const uint32_t tail, const size_t size);
 
+static inline bool is_empty(const uint32_t head, const uint32_t tail);
+
+static inline bool is_full(const uint32_t head, const uint32_t tail);
+
 bool ring_buffer_init(ring_buffer_t* rb, uint8_t *buf, size_t size)
 {
 	if (!rb || !buf || size <= 0)
@@ -30,7 +34,7 @@ bool ring_buffer_write_byte(ring_buffer_t* rb, const uint8_t data)
 	}
 
 
-	if (rb->head + size == rb->tail)
+	if (is_full(rb->head + size, rb->tail))
 	{
 		// full case
 		return false;
@@ -43,7 +47,7 @@ bool ring_buffer_write_byte(ring_buffer_t* rb, const uint8_t data)
 
 bool ring_buffer_write(ring_buffer_t *rb, const uint8_t *data, size_t size)
 {
-	if (!rb || !data)
+	if (!rb || !data || size <= 0)
 	{
 		return false;
 	}
@@ -55,6 +59,12 @@ bool ring_buffer_write(ring_buffer_t *rb, const uint8_t *data, size_t size)
 
 	// split into two memcpy's each copying one contigous memory space
 	size_t len_first_seg = rb->size - rb->head;
+
+	if (len_first_seg > size)
+	{
+		len_first_seg = size;
+	}
+
 	memcpy(&rb->buf[rb->head], data, len_first_seg);
 	memcpy(&rb->buf[0], data + len_first_seg, size - len_first_seg);
 
@@ -62,9 +72,52 @@ bool ring_buffer_write(ring_buffer_t *rb, const uint8_t *data, size_t size)
 	return true;
 }
 
-bool ring_buffer_pop_byte(ring_buffer_t* rb, uint8_t* data);
+bool ring_buffer_pop_byte(ring_buffer_t* rb, uint8_t* data)
+{
+	if (!rb || !data)
+	{
+		return false;
+	}
 
-bool ring_buffer_pop(ring_buffer_t* rb, uint8_t *data, size_t size);
+	if (is_empty(rb->head, rb->tail))
+	{
+		return false;
+	}
+
+	*data = rb->tail;
+
+	rb->tail++;
+
+	return true;
+
+}
+
+bool ring_buffer_pop(ring_buffer_t* rb, uint8_t *data, size_t size)
+{
+	if (!rb || !data || size <= 0)
+	{
+		return false;
+	}
+
+	if (size > get_total_elements(rb->head, rb->tail))
+	{
+		return false;
+	}
+
+	size_t len_first_seg = rb->size - rb->tail;
+
+	if (len_first_seg > size)
+	{
+		len_first_seg = size;
+	}
+
+	memcpy(data, &rb->buf[rb->tail], len_first_seg);
+	memcpy(data + len_first_seg, &rb->buf[0], size - len_first_seg);
+
+	rb->tail = (rb->tail + size) % rb->size;
+
+	return true;
+}
 
 static inline size_t get_free_space(const uint32_t head, const uint32_t tail, const size_t size)
 {
@@ -74,4 +127,14 @@ static inline size_t get_free_space(const uint32_t head, const uint32_t tail, co
 static inline size_t get_total_elements(const uint32_t head, const uint32_t tail, const size_t size)
 {
 	return (head + size - tail) % size;
+}
+
+static inline bool is_empty(const uint32_t head, const uint32_t tail)
+{
+	return head == tail;
+}
+
+static inline bool is_full(const uint32_t head, const uint32_t tail)
+{
+	return (head+1) >= tail;
 }
